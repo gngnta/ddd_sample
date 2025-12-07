384c93d105e638bca62197d10d5f9cd5992b1726
ota <gengenta2141@gmail.com>
2025-12-07 01:45:24 +0000

    fix: update file paths in SQL data import statements to correct locations

--- .devcontainer/init_db/02 _data.sql
+++ .devcontainer/init_db/02 _data.sql
@@ -1,11 +1,11 @@
 COPY categories (id, name, description)
-FROM '/workspace/.devcontainer/init_db/csv/categories.csv'
+FROM '/docker-entrypoint-initdb.d/csv/categories.csv'
 WITH (FORMAT CSV, HEADER TRUE);
 
 COPY questions (id, category_id, question_text)
-FROM '/workspace/.devcontainer/init_db/csv/questions.csv'
+FROM '/docker-entrypoint-initdb.d/csv/questions.csv'
 WITH (FORMAT CSV, HEADER TRUE);
 
 COPY question_choices (id, question_id, choice_text, is_correct)
-FROM '/workspace/.devcontainer/init_db/csv/question_choices.csv'
+FROM '/docker-entrypoint-initdb.d/csv/question_choices.csv'
 WITH (FORMAT CSV, HEADER TRUE);
