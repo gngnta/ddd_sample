978d2182cc7314dc76cb09825e07d08ba2e90704
ota <gengenta2141@gmail.com>
2025-12-09 01:18:54 +0000

    feat: add OpenAPI definition for Quiz API in DemoApplication

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(info = @Info(title = "Quiz API", version = "1.0"))
 @SpringBootApplication
 public class DemoApplication {
 
